990acc153e669c2af486b3f3f0ae9c99ae6c3b08
LeonidFrlv <pro-kapr@mail.ru>
2023-12-10 10:06:03 +0300

    1.0. Небольшое исправление.

--- src/main/java/org/s1queence/plugin/utils/Utils.java
+++ src/main/java/org/s1queence/plugin/utils/Utils.java
@@ -6,8 +6,6 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
-import static java.util.Optional.ofNullable;
-
 public class Utils {
     public static boolean isNbtChangeException(ItemStack item, List<String> exceptions) {
         return exceptions.contains(item.getType().toString()) || (item.hasItemMeta() && item.getItemMeta() != null && item.getItemMeta().hasDisplayName() && exceptions.contains(item.getItemMeta().getDisplayName()));
@@ -18,7 +16,7 @@ public class Utils {
         String title = "[" + ChatColor.GOLD + "NBTChangeCommands" + ChatColor.WHITE + "]";
 
         if (msg == null)  {
-            String nullMsgError = ofNullable(config.getString("msg_is_null")).orElse("&6%plugin% FATAL ERROR." + " We recommend that you delete the text.yml file from the plugin folder and use reload config.");
+            String nullMsgError = "&6%plugin% FATAL ERROR. We recommend that you delete the text.yml file from the plugin folder and use reload config.";
             return ChatColor.translateAlternateColorCodes('&', nullMsgError.replace("%plugin%", title).replace("%msg_path%", path));
         }
 
